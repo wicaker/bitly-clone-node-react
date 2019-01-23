b3837900eecb167dbac4d566eb3d4e224889a55a
wicak <wicaksanaigede@gmail.com>
2019-01-23 16:51:59 +0800

    adding websocket

--- routes/api/shorters.js
+++ routes/api/shorters.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const validUrl = require('valid-url');
 const shortid = require('shortid');
+const cheerio = require('cheerio');
+const axios = require('axios');
 
 //Connect to socket io
 const io = require('../../socket');
@@ -22,6 +24,7 @@ const Tracker = require('../../models/Trackers');
 // @access Private
 router.get('/api/shorters', passport.authenticate('jwt', {session : false}), (req, res) => {
   const errors = {};
+  let data =''
   Shorter
     .find({ user: req.user.id })
     .then( shorter => {
@@ -30,11 +33,10 @@ router.get('/api/shorters', passport.authenticate('jwt', {session : false}), (re
         return res.status(404).json(errors);
       }
       res.json(shorter)
-      io.getIO().emit('shorter',{shorter : 'shorter'})
     })
     .catch(err => res.status(404).json(err));
+    
 });
-
 
 
 
@@ -42,7 +44,6 @@ router.get('/api/shorters', passport.authenticate('jwt', {session : false}), (re
 // @desc POST url to shorting
 // @access Private
 router.post('/api/shorters', passport.authenticate('jwt', {session : false}), (req, res) => {
-  const errors = {};
   const { originalUrl } = req.body;
   const baseurl = req.headers.host;
   const urlCode = shortid.generate();
@@ -54,21 +55,26 @@ router.post('/api/shorters', passport.authenticate('jwt', {session : false}), (r
       .findOne({urlCode: urlCode})
       .then(urlcode => {
         if(urlcode){
-          errors.urlCode = 'UrlCode already exists, please generate again';
-          return res.status(400).json(errors); //response if already
+          return res.status(400).json({"messange": "please generate again"}); //response if already
         }
-        const newUrl = new Shorter({
-          user: req.user,
-          title: req.body.title || null,
-          originalUrl : req.body.originalUrl,
-          urlCode,
-          shortUrl:baseurl+'/'+urlCode,
-          totalClicks: 0
-        })
-        newUrl
-          .save()
-          .then(newUrl => res.json(newUrl)) //response
-          .catch(err => console.log(err))
+        axios.get(req.body.originalUrl)
+        .then((response) => {
+            const html = response.data;
+            const $ = cheerio.load(html); 
+            const title= $('title').text();
+            const newUrl = new Shorter({
+              user: req.user,
+              title: title,
+              originalUrl : req.body.originalUrl,
+              urlCode,
+              shortUrl:baseurl+'/'+urlCode,
+              totalClicks: 0
+            })
+            newUrl
+              .save()
+              .then(newUrl => res.json(newUrl)) //response
+              .catch(err => console.log(err))
+        });
       })
   }
 });
@@ -98,6 +104,7 @@ router.get("/:code", async (req, res) => {
             .save()
             .then(()=>console.log('success'))
           .catch(err => console.log(err));
+          io.getIO().emit('shorter',urlCode)
           return res.redirect(urlCode.originalUrl);
         }
       })
